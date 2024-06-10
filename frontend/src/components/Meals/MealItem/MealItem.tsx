203fb78fd36d8d96f252c73433fad402776eaae7
Dominik Matras <matrasss02@gmail.com>
2024-06-10 15:19:35 +0200

    Adding photoPath fetching to frontend

--- frontend/src/components/Meals/MealItem/MealItem.tsx
+++ frontend/src/components/Meals/MealItem/MealItem.tsx
@@ -1,35 +1,35 @@
-import toast from "react-hot-toast";
-import { useOrderContext } from "../../../context/OrderContext";
-import { Meal } from "../MealsList/MealsList";
-import "./MealItem.less";
-import { useNavigate } from "react-router-dom";
-import { FcInfo } from "react-icons/fc";
+import toast from 'react-hot-toast'
+import { useOrderContext } from '../../../context/OrderContext'
+import { Meal } from '../MealsList/MealsList'
+import './MealItem.less'
+import { useNavigate } from 'react-router-dom'
+import { FcInfo } from 'react-icons/fc'
 
-const MealItem = ({ id, name, price }: Meal) => {
-  const navigate = useNavigate();
-  const { dispatch, startOrder } = useOrderContext();
+const MealItem = ({ id, name, price, photoPath }: Meal) => {
+	const navigate = useNavigate()
+	const { dispatch, startOrder } = useOrderContext()
 
-  const addMealToOrderHandler = () => {
-    if (!startOrder) {
-      toast("You have to choose a table first!", {icon: <FcInfo />});
-      navigate('/tables');
-      return;
-    }
-    dispatch({ type: "ADD_MEAL_TO_ORDER", payload: { id, name, price, quantity: 1 } });
-  };
+	const addMealToOrderHandler = () => {
+		if (!startOrder) {
+			toast('You have to choose a table first!', { icon: <FcInfo /> })
+			navigate('/tables')
+			return
+		}
+		dispatch({ type: 'ADD_MEAL_TO_ORDER', payload: { id, name, price, quantity: 1 } })
+	}
 
-  return (
-    <div className="meal-item">
-      <img src="/burger.jpg" alt="burger image" className="meal-item__img" />
-      <h5 className="meal-item__title">{name}</h5>
-      <p className="meal-item__price">
-        <span>${price}</span>
-      </p>
-      <button className="meal-item__btn" onClick={addMealToOrderHandler}>
-        Add dish
-      </button>
-    </div>
-  );
-};
+	return (
+		<div className="meal-item">
+			<img src={photoPath} alt="burger image" className="meal-item__img" />
+			<h5 className="meal-item__title">{name}</h5>
+			<p className="meal-item__price">
+				<span>${price}</span>
+			</p>
+			<button className="meal-item__btn" onClick={addMealToOrderHandler}>
+				Add dish
+			</button>
+		</div>
+	)
+}
 
-export default MealItem;
+export default MealItem
--- frontend/src/components/Meals/MealsList/MealsList.tsx
+++ frontend/src/components/Meals/MealsList/MealsList.tsx
@@ -1,53 +1,60 @@
-import { useState } from "react";
-import { PAGE_SIZE } from "../../../utils/constants";
+import { useState } from 'react'
+import { PAGE_SIZE } from '../../../utils/constants'
 
-import { useMeals } from "../../../hooks/Meals/useMeals";
-import MealItem from "../MealItem/MealItem";
-import Pagination from "../../ui/Pagination/Pagination";
-import Spinner from "../../ui/Spinner/Spinner";
-import "./MealsList.less";
+import { useMeals } from '../../../hooks/Meals/useMeals'
+import MealItem from '../MealItem/MealItem'
+import Pagination from '../../ui/Pagination/Pagination'
+import Spinner from '../../ui/Spinner/Spinner'
+import './MealsList.less'
 
 type MealsListProps = {
-  searchedValue: string;
-};
+	searchedValue: string
+}
 
 export type Meal = {
-  id: number;
-  name: string;
-  price: number;
-};
+	id: number
+	name: string
+	price: number
+	photoPath: string
+}
 
 const MealsList = ({ searchedValue }: MealsListProps) => {
-  const [page, setPage] = useState(1);
-  const { meals, isLoading } = useMeals();
-  if (isLoading || !meals) return <Spinner />;
+	const [page, setPage] = useState(1)
+	const { meals, isLoading } = useMeals()
+	if (isLoading || !meals) return <Spinner />
 
-  const pagedMeals = meals.slice(page * PAGE_SIZE - PAGE_SIZE, PAGE_SIZE * page);
+	const pagedMeals = meals.slice(page * PAGE_SIZE - PAGE_SIZE, PAGE_SIZE * page)
 
-  const filteredMeals = pagedMeals.filter((meal: Meal) =>
-    meal.name.toLowerCase().includes(searchedValue)
-  );
+	const filteredMeals = pagedMeals.filter((meal: Meal) =>
+		meal.name.toLowerCase().includes(searchedValue)
+	)
 
-  return (
-    <>
-      <div className="meals-list">
-        <ul className="meals-list__list">
-          {filteredMeals.map((meal: Meal) => {
-            return (
-              <MealItem key={meal.id} id={meal.id} name={meal.name} price={meal.price} />
-            );
-          })}
-        </ul>
-      </div>
-      <Pagination
-        className="meals-list__pagination"
-        pageSize={PAGE_SIZE}
-        count={meals.length}
-        setPage={setPage}
-        page={page}
-      />
-    </>
-  );
-};
+	return (
+		<>
+			<div className="meals-list">
+				<ul className="meals-list__list">
+					{filteredMeals.map((meal: Meal) => {
+						return (
+							<MealItem
+								key={meal.id}
+								id={meal.id}
+								name={meal.name}
+								price={meal.price}
+								photoPath={meal.photoPath}
+							/>
+						)
+					})}
+				</ul>
+			</div>
+			<Pagination
+				className="meals-list__pagination"
+				pageSize={PAGE_SIZE}
+				count={meals.length}
+				setPage={setPage}
+				page={page}
+			/>
+		</>
+	)
+}
 
-export default MealsList;
+export default MealsList
