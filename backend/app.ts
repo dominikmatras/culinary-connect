ecc73f8ef6b96f2e002d309402a036b52db16e07
Dominik Matras <matrasss02@gmail.com>
2024-06-28 18:25:15 +0200

    chore: Update CORS origin to include production URL

--- backend/app.ts
+++ backend/app.ts
@@ -21,7 +21,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+		origin: [
+			'http://127.0.0.1:5173',
+			'http://localhost:5173',
+			'https://culinary-connect.netlify.app',
+		],
 		credentials: true,
 	})
 )
