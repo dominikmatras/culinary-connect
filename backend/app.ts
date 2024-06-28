60ec8bf94d222f368086b63bc60eb24e9f636e74
Dominik Matras <matrasss02@gmail.com>
2024-06-28 23:49:16 +0200

    chore: Update CORS origin and HOST constant

--- backend/app.ts
+++ backend/app.ts
@@ -21,16 +21,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: [
-			'http://127.0.0.1:5173',
-			'http://localhost:5173',
-			'https://culinary-connect.netlify.app',
-		],
+		origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
 		credentials: true,
 	})
 )
-
-app.options('*',cors())
 
 app.use('/api/v1/meals', mealRouter)
 app.use('/api/v1/users', userRouter)
