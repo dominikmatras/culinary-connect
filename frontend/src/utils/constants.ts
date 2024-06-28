b2eb761984e9a69c580a36954fb3862030f08b83
Dominik Matras <matrasss02@gmail.com>
2024-06-28 19:20:06 +0200

    chore: Update HOST constant to use new API URL

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const PAGE_SIZE = 10
 export const TAX_VALUE = 5
-export const HOST = 'http://127.0.0.1:3000' ?? 'https://culinary-connect.netlify.app'
+export const HOST = 'http://127.0.0.1:3000' ?? 'https://culinary-connect-api.netlify.app'
 export const API_VERSION = '/api/v1'
