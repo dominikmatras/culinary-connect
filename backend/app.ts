7d4673803f60c9fe235826c2605793e24f4a4b33
Dominik Wędzina <dominik.wedzina@onet.pl>
2024-11-12 09:11:23 +0100

    add host to cors

--- backend/app.ts
+++ backend/app.ts
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+		origin: ['http://127.0.0.1:5173', 'http://localhost:5173', "https://culinary-connect-mu.vercel.app"],
 		credentials: true,
 	})
 )
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
